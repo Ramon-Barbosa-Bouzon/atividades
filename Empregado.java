5ab65fccd4971e84837d8ff9c24051712d0ab568
Ramon Bouzon <ramonbouzon@hotmail.com>
2020-09-03 21:21:27 -0300

    Metodo de calcular salario pronto e Teste pronto

--- Empregado.java
+++ Empregado.java
@@ -1,13 +1,15 @@
-public class Empregado{
+public class Empregado {
     private String nome;
     private int idade;
-    private int tipo;
+    private double comicao;
     private double salario;
     private double bonus;
+    private int tipo;
 
     public void setIdade(int idade) {
         this.idade = idade;
     }
+
     public int getIdade() {
         return idade;
     }
@@ -15,12 +17,62 @@ public class Empregado{
     public void setNome(String nome) {
         this.nome = nome;
     }
+
     public String getNome() {
         return nome;
     }
-    
-    public Empregado(){
+
+    public double getBonus() {
+        return bonus;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public double getComicao() {
+        return comicao;
+    }
+
+    public int getTipo() {
+        return tipo;
+    }
+
+    public void setBonus(double bonus) {
+        this.bonus = bonus;
+    }
+
+    public void setComicao(double comicao) {
+        this.comicao = comicao;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public void setTipo(int tipo) {
+        this.tipo = tipo;
+    }
+
+    public double CalculaSalario(){
+
+        switch( getTipo() ){
+
+            case 1:
+            return salario;
+            
+            case 2:
+            return salario * comicao;
+            
+            case 3:
+            return salario + bonus;
+
+            default:
+            return 0;
         
+        }
+        
+
     }
 
 }
--- TestEmpregado.java
+++ TestEmpregado.java
@@ -4,6 +4,22 @@ public class TestEmpregado {
         Empregado e2 = new Empregado();
         Empregado e3 = new Empregado();
         //e agora, como configurar o tipo deles
+
+        e1.setTipo(1);
+        e2.setTipo(2);
+        e3.setTipo(3);
+
+        e1.setSalario(2000);
+
+        e2.setSalario(1700);
+        e2.setComicao(0.2);
+
+        e3.setSalario(1500);
+        e3.setBonus(400);
+
+        System.out.println(e1.CalculaSalario());
+        System.out.println(e2.CalculaSalario());
+        System.out.println(e3.CalculaSalario());
         
     }
     
